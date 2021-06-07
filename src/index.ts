29c979e95e0e8591a8bd35af6f6c1488b1020d11
John O'Hara <johnoharaa@gmail.com>
2021-06-07 01:36:38 -0700

    Added page transitions, improved styles, finished darkmode toggle.

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,69 @@ import '@fortawesome/fontawesome-free';
 
 // setup glide carousel
 import Glide from '@glidejs/glide';
-
 new Glide('.glide', {autoplay: 10000}).mount();
 
 // some DOM constants
 const body           = document.getElementsByTagName("body")[0];
+const pageWrapper    = document.getElementById("page-wrapper")!;
+const pageBody       = document.getElementById("page-body-wrapper")!;
+
 const darkModeToggle = document.getElementById("darkmode-toggle");
+const fadeOutToggle  = document.getElementById("fadeout");
+const navLinks       = document.getElementsByClassName("nav-link");
+const logoLink       = document.getElementById("logo-link") as HTMLAnchorElement;
 
 // wait until everything is loaded
-// this isn't working how I'd like
-document.addEventListener("load", () => {
-  body.style.opacity = "1";
+window.addEventListener("load", () => {
+  pageWrapper.style.opacity = "1";
 });
 
+
+
+
+// setup nav transitions
+for (let link of navLinks) {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let link = <HTMLAnchorElement> event.target;
+
+    let pageWrapperCallback = () => {
+      fetch(link.href)
+      .then(response => response.text())
+      .then(data =>  data)
+      .then(data => {
+        pageWrapper.removeEventListener("transitionend", pageWrapperCallback);
+        pageBody.innerHTML = data;
+        pageWrapperOpacityToggle();
+      });
+    }
+
+    pageWrapperOpacityToggle();
+    pageWrapper.addEventListener("transitionend", pageWrapperCallback);
+  });
+}
+
+logoLink?.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // man, I don't like how this is reused, but I don't know how else to do this.
+  let pageWrapperCallback = () => {
+    fetch(logoLink.href)
+    .then(response => response.text())
+    .then(data =>  data)
+    .then(data => {
+      pageWrapper.removeEventListener("transitionend", pageWrapperCallback);
+      pageBody.innerHTML = data;
+      new Glide('.glide', {autoplay: 10000}).mount();
+
+      setTimeout(() => pageWrapperOpacityToggle(), 500);
+    });
+  }
+
+  pageWrapperOpacityToggle();
+  pageWrapper.addEventListener("transitionend", pageWrapperCallback);
+});
 
 // setup darkmode
 let darkModeActive = localStorage.getItem("darkModeActive") == "true";
@@ -38,3 +88,14 @@ darkModeToggle?.addEventListener("click", (event) => {
   localStorage.setItem("darkModeActive", `${darkModeActive}`);
 });
 
+fadeOutToggle?.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  console.log(pageWrapper.style.opacity);
+
+  pageWrapperOpacityToggle();
+});
+
+function pageWrapperOpacityToggle() {
+  pageWrapper.style.opacity = pageWrapper.style.opacity === "1" ? "0" : "1";
+}
